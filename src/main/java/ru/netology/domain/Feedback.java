97558ebe7d0e64d96e58e15286e4f3d34def0daf
Lera Lyakisheva <leralyakisheva@gmail.com>
2021-12-28 13:22:25 +0300

    Version 1

---
+++ src/main/java/ru/netology/domain/Feedback.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Feedback {
+    private int likesInfo;
+    private String commentsInfo;
+    private  int viewsInfo;
+    private int repostsInfo;
+
+    // getter+setter;
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String imageUrl;
+    private String text;
+    private String link;
+
+    // getter+setter;
+}
