fd7de66a46ca2418034d7afe3cf863128f0ac66e
Lera Lyakisheva <leralyakisheva@gmail.com>
2021-12-30 01:18:39 +0300

    PostManager changed

--- src/main/java/гu/netology/manager/PostManager.java
+++ src/main/java/гu/netology/manager/PostManager.java
@@ -1,2 +1,15 @@
-package гu.netology.manager;public class PostManager {
+package гu.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public String[] searchWallPosts (int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void deleteWallPosts (int ownerId, int postId) {
+
+    }
+
 }
